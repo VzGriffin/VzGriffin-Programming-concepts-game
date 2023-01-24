123cf03963d12b380610d5bd68e63c560db00d72
VzGriffin <Mylesleyton12@gmail.com>
2023-01-24 11:05:31 +0000

    Finished with menues

--- Programming-concepts-game/Assets/Finish.cs
+++ Programming-concepts-game/Assets/Finish.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Finish : MonoBehaviour
 {
@@ -16,14 +17,14 @@ public class Finish : MonoBehaviour
         if (collision.gameObject.name == "Player")
         {
             finishSound.Play();
-            CompleteLevel();
+            Invoke("CompleteLevel", 2f);
         }
     }
     
 
     private void CompleteLevel()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
     
 }
